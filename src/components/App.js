7bb3d641a85c29b11eea6059d1fb1fb1f3f6cd72
Egor Gordeev <superstep_007@mail.ru>
2021-03-07 02:05:59 +0300

    finished course

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,39 @@ import AddAppointments from "./AddAppointments";
 import ListAppointments from "./ListAppointments";
 import SearchAppointments from "./SearchAppointments";
 
-import { without } from "lodash";
+import { findIndex, without } from "lodash";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       myAppointments: [],
+      formDisplay: false,
+      orderBy: "petName",
+      orderDir: "asc",
+      queryText: "",
       lastIndex: 0
     };
     this.deleteAppointent = this.deleteAppointent.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
+    this.changeOrder = this.changeOrder.bind(this);
+    this.searchApts = this.searchApts.bind(this);
+    this.updateInfo = this.updateInfo.bind(this);
+  }
+
+  addAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    apt.itemId = this.state.lastIndex + 1;
+    tempApts.unshift(apt);
+    this.setState({
+      myAppointments: tempApts,
+      lastIndex: apt.itemId
+    });
+  }
+
+  toggleForm() {
+    this.setState({ formDisplay: !this.state.formDisplay });
   }
 
   deleteAppointent(apt) {
@@ -41,18 +64,77 @@ class App extends Component {
       });
   }
 
+  changeOrder(order, dir) {
+    this.setState({
+      orderBy: order,
+      orderDir: dir
+    });
+  }
+
+  searchApts(query) {
+    this.setState({
+      queryText: query
+    });
+  }
+
+  updateInfo(name, value, id) {
+    let tempApts = this.state.myAppointments;
+    let aptIndex = findIndex(this.state.myAppointments, {
+      itemId: id
+    });
+    tempApts[aptIndex][name] = value;
+    this.setState({
+      myAppointments: tempApts
+    });
+  }
+
   render() {
+    let order;
+    let filteredApts = this.state.myAppointments;
+    if (this.state.orderDir === "asc") {
+      order = 1;
+    } else {
+      order = -1;
+    }
+
+    filteredApts = filteredApts
+      .sort((a, b) => {
+        if (
+          a[this.state.orderBy].toLowerCase() <
+          b[this.state.orderBy].toLowerCase()
+        ) {
+          return -1 * order;
+        } else {
+          return 1 * order;
+        }
+      })
+      .filter((item) => {
+        return item["petName"]
+          .toLowerCase()
+          .includes(this.state.queryText.toLowerCase());
+      });
+
     return (
       <main className="page bg-white" id="petratings">
         <div className="container">
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddAppointments />
-                <SearchAppointments />
+                <AddAppointments
+                  formDisplay={this.state.formDisplay}
+                  toggleForm={this.toggleForm}
+                  addAppointment={this.addAppointment}
+                />
+                <SearchAppointments
+                  orderBy={this.state.orderBy}
+                  orderDir={this.state.orderDir}
+                  changeOrder={this.changeOrder}
+                  searchApts={this.searchApts}
+                />
                 <ListAppointments
-                  myAppointments={this.state.myAppointments}
+                  myAppointments={filteredApts}
                   deleteAppointent={this.deleteAppointent}
+                  updateInfo={this.updateInfo}
                 />
               </div>
             </div>
--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -9,26 +9,70 @@ class ListAppointments extends Component {
         {this.props.myAppointments.map((item) => (
           <div className="pet-item col media py-3" key={item.itemId}>
             <div className="mr-3">
-              <button className="pet-delete btn btn-sm btn-danger"
-                onClick={()=>this.props.deleteAppointent(item)}>
+              <button
+                className="pet-delete btn btn-sm btn-danger"
+                onClick={() => this.props.deleteAppointent(item)}
+              >
                 <FaTimes />
               </button>
             </div>
 
             <div className="pet-info media-body">
               <div className="pet-head d-flex">
-                <span className="pet-name">{item.petName}</span>
+                <span
+                  className="pet-name"
+                  contentEditable
+                  suppressContentEditableWarning
+                  onBlur={(e) =>
+                    this.props.updateInfo(
+                      "petName",
+                      e.target.innerText,
+                      item.itemId
+                    )
+                  }
+                >
+                  {item.petName}
+                </span>
                 <span className="apt-date ml-auto">
-                <Moment date={item.aptDate} parse="YYYY-MM-dd hh:mm"
-                format="YYYY MMM-D hh:mm"/>
+                  <Moment
+                    date={item.aptDate}
+                    parse="YYYY-MM-dd hh:mm"
+                    format="YYYY MMM-D hh:mm"
+                  />
                 </span>
               </div>
 
               <div className="owner-name">
-                <span className="label-item">Owner: </span>
+                <span
+                  className="label-item"
+                  contentEditable
+                  suppressContentEditableWarning
+                  onBlur={(e) =>
+                    this.props.updateInfo(
+                      "ownerName",
+                      e.target.innerText,
+                      item.itemId
+                    )
+                  }
+                >
+                  Owner:{" "}
+                </span>
                 <span>{item.ownerName}</span>
               </div>
-              <div className="apt-notes">{item.aptNotes}</div>
+              <div
+                className="apt-notes"
+                contentEditable
+                suppressContentEditableWarning
+                onBlur={(e) =>
+                  this.props.updateInfo(
+                    "aptNotes",
+                    e.target.innerText,
+                    item.itemId
+                  )
+                }
+              >
+                {item.aptNotes}
+              </div>
             </div>
           </div>
         ))}
